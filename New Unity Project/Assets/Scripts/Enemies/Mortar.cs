f1aab74acf7617ae749b26456ec688cce69d34ce
Karrottu <dru.tanti@gmail.com>
2020-02-02 13:41:38 +0100

    Fixed Bullet

--- New Unity Project/Assets/Scripts/Enemies/Mortar.cs
+++ New Unity Project/Assets/Scripts/Enemies/Mortar.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mortar : Projectile {
+public class Mortar : MonoBehaviour {
+    public float speed = 30f;
+    public float lifetime = 3f;
+    private Rigidbody _bulletRB;
     [Range(0f, 10f)]
     [Tooltip("Time from the Mortar being fired to dropping from the sky")]
     public float timeToDrop;
@@ -12,7 +15,10 @@ public class Mortar : Projectile {
     public Color hitMarkerColour;
     private bool _dropping = false;
     float timer = 0f;
-    protected override void Start() {
+    private void Awake() {
+        _bulletRB = GetComponent<Rigidbody>();
+    }
+    void Start() {
         hitMarkerCircle.GetComponent<SpriteRenderer>().color = hitMarkerColour;
         hitMarkerRing.GetComponent<SpriteRenderer>().color = hitMarkerColour;
         GameObject ring = Instantiate(hitMarkerRing, new Vector3 (transform.position.x, -0.9f ,transform.position.z),  Quaternion.Euler(90, 0, 0));
@@ -23,7 +29,7 @@ public class Mortar : Projectile {
         Destroy(gameObject, timeToDrop + lifetime);
     }
 
-    protected void Update() {
+    void Update() {
         timer += Time.deltaTime;
         Debug.Log(transform.GetChild(3));
         if(!_dropping) transform.GetChild(3).localScale = Vector3.Lerp(new Vector3(0f, 0f, 0f), new Vector3(2f, 2f, 2f), timer / timeToDrop);
